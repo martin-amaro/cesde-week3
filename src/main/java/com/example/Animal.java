6b56408ef9c41e815a3f96526f8145bdf6137820
Martin <martingadpacks@gmail.com>
2025-03-01 16:29:29 -0500

    Initial commit

---
+++ src/main/java/com/example/Animal.java
@@ -0,0 +1,16 @@
+package com.example;
+
+public class Animal {
+
+    private String name;
+    private String raza;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    String getName() {
+        return name;
+    }
+
+}
---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal gato = new Animal("Gato");
+        System.out.println("El nombre del animal es: " + gato.getName());
+
+        JOptionPane.showInputDialog(null, "Please enter");
+        
+
+
+    }
+}
